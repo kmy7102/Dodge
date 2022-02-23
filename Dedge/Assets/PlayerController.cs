eb8da52996a6443966f5429eb77fa58fb681603a
kmy7102 <kjm36291295@gmail.com>
2022-02-23 17:26:32 +0900

    20220223_Last

---
+++ Dedge/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+
+    public float speed = 8f;
+
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
