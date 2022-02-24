9fc966b4af37ff3afaf319b4c662c4b5a97af5d9
jeongsieun <8sejse8@gmail.com>
2022-02-24 17:32:27 +0900

    20220224

--- Dedge/Assets/PlayerController.cs
+++ Dedge/Assets/PlayerController.cs
@@ -6,16 +6,47 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody playerRigidbody;
 
-    public float speed = 8f;
+    public float speed = 3f;
 
-    void Start()
+    public GameObject my;
+
+    private void Start()
     {
-       
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float lnput = Input.GetAxis("Horizontal");
+        // 키보드 : 'A', <- : 음의방향 : -1.0f
+        // 키보드 : 'D', -> : 양의방향 : +1.0f
+        float zlnput = lnput.GutAxis("Vertical");
+        // 키보드 : 'W',^ : 양의방향 : +1.0f
+        // 키보드 : 'S',  : 음의방향 : -1.0f
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody
+                .AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody
+                .AddForce(0f, 0f, -speed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody
+                .AddForce(-speed, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody
+                .AddForce(speed,0f, 0f);
+        }
+    }
+    void Die()
+    {
+        my.SetActive(false);
     }
 }
